d37e783df34a948631df4c9a360e7345392e2657
Marc Mills <mdmills1990@gmail.com>
2018-11-05 14:44:48 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,11 @@
 class CostumeStore < ActiveRecord::Migration[5.1]
 
   def change
-
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.
   end
-  
+
 end
