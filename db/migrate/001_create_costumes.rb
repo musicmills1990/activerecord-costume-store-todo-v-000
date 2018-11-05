58406465e32d5543e54c68054c77fe9a8a95c04f
Marc Mills <mdmills1990@gmail.com>
2018-11-05 14:39:52 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,5 +1,5 @@
 # Create a class and inherit from ActiveRecord::Migration
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[5.1]
 
   def change
     create_table :costumes do |t|
@@ -10,7 +10,6 @@ class CreateCostumes < ActiveRecord::Migration
       t.timestamps
     end
   end
-  
 end
 # By convention, the class name should match the part of the
 # file name after the number, so in this case:
